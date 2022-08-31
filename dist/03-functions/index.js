c62f6373726d8db7e0ab59425f010d5d3aa8738c
EdgarMTdev <edgarmontiel961@gmail.com>
2022-08-30 19:12:57 -0500

    add: generators, callbacks and types

--- dist/03-functions/index.js
+++ dist/03-functions/index.js
@@ -53,4 +53,76 @@ let x = {
 };
 x.a();
 let func = x.a;
-console.log(func());
+// console.log(func());
+function printMessage(message) {
+    console.log(message, this.name);
+}
+printMessage.call({
+    id: 'abc123',
+    name: 'Edgar',
+    age: 20,
+}, 'hello');
+// The 'this' context of type 'void' is not assignable to method's 'this' of type 'User'
+// printMessage('hello')
+// Generadors: generators
+function* createFibonacci() {
+    let a = 0;
+    let b = 1;
+    while (true) {
+        yield a; // Return, pero con pause
+        // let aux = a
+        // a = b
+        // b = aux + b
+        [a, b] = [b, a + b];
+    }
+}
+let fibonacciGenerator = createFibonacci();
+fibonacciGenerator.next(); // 0
+fibonacciGenerator.next(); // 1
+fibonacciGenerator.next(); // 2
+fibonacciGenerator.next(); // 3
+fibonacciGenerator.next(); // 4
+console.log(fibonacciGenerator.next().value); // 5
+// TODO: Utilizar un generador para generar esta serie: 1,2,3,4,5,6,7
+function* createSequence() {
+    let a = 1;
+    while (true) {
+        yield a;
+        a++;
+    }
+}
+let sequenceGenerator = createSequence();
+sequenceGenerator.next();
+sequenceGenerator.next();
+sequenceGenerator.next();
+console.log(sequenceGenerator.next());
+// Iterable: Cualquier objeto que contenga un propiedad llamada Symbol.iterable
+// Iterator: Cualquier objeto que define un método next
+// Array en su forma base: 
+let numbersIterator = {
+    *[Symbol.iterator]() {
+        for (let n = 1; n <= 10; n++) {
+            yield n;
+        }
+    }
+};
+const arrayIterable = [1, 2, 3, 4, 5];
+arrayIterable[Symbol.iterator];
+// Iterable -> Spread operator, destructuring
+let allNumbersIterator = [...numbersIterator];
+let [one, two, ...rest] = numbersIterator;
+// TODO: crear un iterable con los primeros 10 numeros de la serie de fibonacci
+// Call signatures: 
+// function: es un tipo que describe todas las funciones
+console.log(typeof greet); // (name: string) => string
+function showDate(callback) {
+    // callback(new Date().toISOString())
+    // callback('Hello', new Date())
+    callback('Hello', new Date(), 'abc123');
+}
+// Contextual typing
+showDate((message, numberOrDate, id) => {
+    console.log(message, numberOrDate, id);
+});
+// arrayIterable.map((n: number) => n * 2)
+arrayIterable.map(n => n * 2);
--- src/03-functions/index.ts
+++ src/03-functions/index.ts
@@ -97,4 +97,114 @@ let x = {
 x.a()
 let func = x.a
 
-console.log(func());
+// console.log(func());
+
+function printMessage(this: User, message: string) {
+    console.log(message, this.name);
+}
+
+printMessage.call({
+    id: 'abc123',
+    name: 'Edgar',
+    age: 20,
+}, 'hello')
+
+// The 'this' context of type 'void' is not assignable to method's 'this' of type 'User'
+// printMessage('hello')
+
+// Generadors: generators
+
+function* createFibonacci() {
+    let a = 0
+    let b = 1
+
+    while (true) {
+        yield a; // Return, pero con pause
+        // let aux = a
+        // a = b
+        // b = aux + b
+        [a, b] = [b, a + b]
+    }
+}
+
+
+let fibonacciGenerator = createFibonacci()
+fibonacciGenerator.next() // 0
+fibonacciGenerator.next() // 1
+fibonacciGenerator.next() // 2
+fibonacciGenerator.next() // 3
+fibonacciGenerator.next() // 4
+console.log(fibonacciGenerator.next().value)// 5
+
+
+// TODO: Utilizar un generador para generar esta serie: 1,2,3,4,5,6,7
+function* createSequence() {
+    let a = 1
+
+    while (true) {
+        yield a;
+        a++
+    }
+}
+
+let sequenceGenerator = createSequence()
+sequenceGenerator.next()
+sequenceGenerator.next()
+sequenceGenerator.next()
+console.log(sequenceGenerator.next());
+
+
+// Iterable: Cualquier objeto que contenga un propiedad llamada Symbol.iterable
+// Iterator: Cualquier objeto que define un método next
+
+// Array en su forma base: 
+let numbersIterator = {
+    *[Symbol.iterator]() {
+        for (let n = 1; n <= 10; n++) {
+            yield n
+        }
+    }
+}
+
+const arrayIterable = [1, 2, 3, 4, 5]
+arrayIterable[Symbol.iterator]
+
+// Iterable -> Spread operator, destructuring
+let allNumbersIterator = [...numbersIterator]
+let [one, two, ...rest] = numbersIterator
+
+// TODO: crear un iterable con los primeros 10 numeros de la serie de fibonacci
+
+// Call signatures: 
+// function: es un tipo que describe todas las funciones
+console.log(typeof greet) // (name: string) => string
+
+// type Callback = (message: string) => void
+// Sobrecarga de métodos
+type Callback = {
+    (message: string): void
+    (message: string, date: Date): void
+    (message: string, date: Date, id: String): void
+    (message: string, date: Date, number: number, id: String): void
+}
+
+function showDate(callback: Callback): void {
+    // callback(new Date().toISOString())
+    // callback('Hello', new Date())
+    callback('Hello', new Date(), 12, 'abc123')
+}
+
+// Contextual typing
+showDate((
+    message: string,
+    numberOrDate?: Date | number,
+    id?
+) => {
+    console.log(message, numberOrDate, id) // -> Hello 2022-08-31T00:05:31.003Z 12
+    console.log(message, numberOrDate, id) // -> Hello 2022-08-31T00:05:31.003Z abc123
+})
+
+// arrayIterable.map((n: number) => n * 2)
+arrayIterable.map(n => n * 2)
+
+// TODO: Investigar sobre polimorfismo y generics
